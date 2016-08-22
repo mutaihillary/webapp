bd50ce7c9866f9281d51e3ae788135a7b2530d17
mutaihillary <mutaihillary@yahoo.com>
2016-08-22 18:28:24 +0300

    update

--- views.py
+++ views.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import render_template, request
+from models import Category, Todo, Priority, db
 
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/')
+def list_all():
+    return render_template(
+        'list.html',
+        categories=Category.query.all(),
+        todos=Todo.query.join(Priority).order_by(Priority.value.desc())
